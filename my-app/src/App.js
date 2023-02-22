3f4bc5014598a2e21651068b2edb6f2f1c110d54
AlexanderJWard <alexjamesward@hotmail.com>
2023-02-22 09:33:05 +0000

    Add rendering lists and map through array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,16 +6,22 @@ import EventsClass from './Components/EventsClass';
 import EventsFunctional from './Components/EventsFunctional';
 import ConditionalRenderingClass from './Components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './Components/ConditionalRenderingFunctional';
+import NestingComponents from './Components/NestingComponents';
+import MethodsAsPropsParent from './Components/MethodsAsPropsParent';
+import RenderingLists from './Components/RenderingLists';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="Nice to meet you" name="Mike" age="32"/>
+      {/* <FunctionalGreetingWithProps greeting="Nice to meet you" name="Mike" age="32"/>
       <StatefulGreeting greeting="I'm a stateful greeting component" name="Bob" />
       <EventsClass/>
       <EventsFunctional/>
       <ConditionalRenderingClass/>
       <ConditionalRenderingFunctional connected={true}/>
+      <NestingComponents/>
+      <MethodsAsPropsParent/> */}
+      <RenderingLists/>
     </div>
   );
 }
---
+++ my-app/src/Components/MethodsAsPropsParent.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import MethodsAsPropsChild from './MethodsAsPropsChild'
+
+class MethodsAsPropsParent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
+
+    toggleSignIn = () => {
+        this.setState((prevState) => ({
+        isLoggedIn: !prevState.isLoggedIn,
+        }));
+    }
+
+    // handleSignIn = () => {
+    //     this.setState({
+    //         isLoggedIn: true
+    //     })
+    //     console.log(this)
+    // }
+
+    // handleSignOut = () => {
+    //     this.setState({
+    //         isLoggedIn: false
+    //     })
+    //     console.log(this)
+    // }
+
+    render() {
+        return (
+            <div>
+                <MethodsAsPropsChild 
+                    isLoggedIn={this.state.isLoggedIn} 
+                    handleSignIn={this.handleSignIn}
+                    handleSignOut={this.handleSignOut}
+                    toggleSignIn={this.toggleSignIn}
+                />
+            </div>
+        )
+    }
+}
+
+export default MethodsAsPropsParent
